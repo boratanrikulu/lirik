be4755d75fa3183efe7281ada8e638b473827d49
boratanrikulu <boratanrikulu@gmail.com>
2020-04-15 15:44:48 +0300

    Add expire times to cookies

--- controllers/helpers/cookie_helper.go
+++ controllers/helpers/cookie_helper.go
@@ -8,14 +8,19 @@ import (
 )
 
 func SetTokenCookies(r models.RefreshAndAccessTokens, w http.ResponseWriter) {
+	oneMonth := time.Hour * 24 * 30
 	cookies := []http.Cookie {
 		http.Cookie {
 			Name: "AccessToken",
 			Value: r.Response.AccessToken,
+			// MaxAge of access token is 1 hour.
+			MaxAge: r.Response.ExpiresIn,
 		},
 		http.Cookie {
 			Name: "RefreshToken",
 			Value: r.Response.RefreshToken,
+			// We will set max age for refresh token 1 month.
+			MaxAge: int(oneMonth.Seconds()),
 		},
 	}
 	for _, cookie := range cookies {
