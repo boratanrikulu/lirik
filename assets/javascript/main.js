bf7e9315941f044ed81f509c26438822080713e7
Aykut Kardaş <aykutkrds@gmail.com>
2021-01-08 01:17:36 +0300

    openTab function refactored.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,15 +1,18 @@
-function openTab(evt, tabName) {
-  var i, x, tablinks;
-  x = document.getElementsByClassName("card-content");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+function openTab(event, tabName) {
+  var cardContentElems = document.getElementsByClassName("card-content");
+  var tabLinkElems = document.getElementsByClassName("tab");
+  var currentContentEl = document.getElementById(tabName);
+
+  for (cardContentEl of cardContentElems) {
+    cardContentEl.style.display = "none";
   }
-  tablinks = document.getElementsByClassName("tab");
-  for (i = 0; i < x.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" is-active", "");
+
+  for (tabLinkEl of tabLinkElems) {
+    tabLinkEl.classList.remove("is-active");
   }
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " is-active";
+
+  currentContentEl.style.display = "block";
+  event.currentTarget.classList.add("is-active");
 }
 
 async function getCurrentSongID() {
